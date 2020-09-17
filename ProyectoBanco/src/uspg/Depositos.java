5d9644f74f432655363b1b39deed6549de41434a
MarioZavala <mrzavalat@alumno.uspg.edu.gt>
2020-09-17 14:47:25 -0600

    Se agregaron cambios en Depositos

--- ProyectoBanco/src/uspg/Depositos.java
+++ ProyectoBanco/src/uspg/Depositos.java
@@ -17,13 +17,13 @@ public class Depositos extends Menu {
         System.out.println("********************");
        System.out.println("Ingresar el monto de deposito");
        
-       Depositos();
+       Deposito1();
        transacciones=getSaldo1();
         
        setSaldo1(transacciones+deposito1);
        
         System.out.println("*******************");
-        System.out.println("Se deposito a su cuenta: Q"+depositos);
+        System.out.println("Se deposito a su cuenta: Q"+deposito1);
         System.out.println("*******************");
         System.out.println("Su actual saldo es de: Q"+getSaldo1());
         System.out.println("*******************");
@@ -34,13 +34,13 @@ public class Depositos extends Menu {
        System.out.println("********************");
        System.out.println("Ingresar el monto de deposito");
        
-       Depositos();
+       Deposito2();
        transacciones=getSaldo2();
         
        setSaldo2(transacciones+deposito2);
        
         System.out.println("*******************");
-        System.out.println("Se deposito a su cuenta: Q"+depositos);
+        System.out.println("Se deposito a su cuenta: Q"+deposito2);
         System.out.println("*******************");
         System.out.println("Su actual saldo es de: Q"+getSaldo2());
         System.out.println("*******************");
@@ -51,13 +51,13 @@ public class Depositos extends Menu {
         System.out.println("********************");
        System.out.println("Ingresar el monto de deposito");
        
-       Depositos();
+       Deposito3();
        transacciones=getSaldo3();
         
        setSaldo3(transacciones+deposito1);
        
         System.out.println("*******************");
-        System.out.println("Se deposito a su cuenta: Q"+depositos);
+        System.out.println("Se deposito a su cuenta: Q"+deposito3);
         System.out.println("*******************");
         System.out.println("Su actual saldo es de: Q"+getSaldo3());
         System.out.println("*******************");
--- ProyectoBanco/src/uspg/Menu.java
+++ ProyectoBanco/src/uspg/Menu.java
@@ -12,46 +12,228 @@ import java.util.Scanner;
  * @author marit
  */
 public abstract class Menu {
+
     Scanner scn = new Scanner(System.in);
 
     public int Saldo1 = 500;
     public int Saldo2 = 1000;
     public int Saldo3 = 1500;
-    
+
     protected int transacciones, deposito1, deposito2, deposito3;
-    
+
     public abstract void Transaccion1();
+
     public abstract void Transaccion2();
+
     public abstract void Transaccion3();
-
-
-
 
     public void saldo() {
         int opcion1 = 0;
-        
-        System.out.println("opcción");
+        int opcion2 = 0;
+        int bandera = 0;
+
+        System.out.println("Opción usuario");
         opcion1 = scn.nextInt();
 
         if (opcion1 == 1) {
-            
-            Menu user1 = new Saldo();
-            user1.Transaccion1();
-            
-        }  else if (opcion1 == 2) {
-            
-                Menu user2 = new Saldo();
-                user2.Transaccion2();
-                
-            } else if (opcion1 == 3) {
-                
-                Menu user3 = new Saldo();
-                user3.Transaccion3();
-                
-            }
-        
-    }
 
+            do {
+
+            //Bucle menú 
+                System.out.println("-----Por favor seleccione una opción-----");
+                System.out.println("    1.Deposito de efectivo");
+                System.out.println("    2.Retiro de dinero");
+                System.out.println("    3.Saldo en la cuenta");
+                System.out.println("    4.Clientes");
+                System.out.println("    5.Salir");
+
+                System.out.println("-----------------------------------------");
+
+                System.out.print("-Opción: ");
+                opcion2 = scn.nextInt();
+
+                if (opcion2 == 1) {
+
+                    System.out.println("   ---Deposito--- ");
+
+                    Menu deposito = new Depositos();
+                    deposito.Transaccion1();
+
+                } else if (opcion2 == 2) {
+                    System.out.println("   ---Retiros--- ");
+
+                //Menu retiros = new Retiros();
+                    //retiros.Transaccion1();
+                } else if (opcion2 == 3) {
+                    System.out.println("   ---Saldo--- ");
+
+                    Menu saldo = new Saldo();
+                    saldo.Transaccion1();
+
+                } else if (opcion2 == 4) {
+                    System.out.println("   ---Clientes--- ");
+
+                 //Menu clientes = new Clientes();
+                    //Clientes.Transaccion1();
+                } else if (opcion2 == 5) {
+                    System.out.println("---------------------");
+                    System.out.println("Gracias vuelva pronto");
+                    System.out.println("---------------------");
+                    bandera = 2;
+                }
+
+            } while (bandera != 2);
+
+        } else if (opcion1 == 2) {
+
+            
+            do {
+
+            //Bucle menú 
+                System.out.println("-----Por favor seleccione una opción-----");
+                System.out.println("    1.Deposito de efectivo");
+                System.out.println("    2.Retiro de dinero");
+                System.out.println("    3.Saldo en la cuenta");
+                System.out.println("    4.Clientes");
+                System.out.println("    5.Salir");
+
+                System.out.println("-----------------------------------------");
+
+                System.out.print("-Opción: ");
+                opcion2 = scn.nextInt();
+
+                if (opcion2 == 1) {
+
+                    System.out.println("   ---Deposito--- ");
+
+                    Menu deposito = new Depositos();
+                    deposito.Transaccion1();
+
+                } else if (opcion2 == 2) {
+                    System.out.println("   ---Retiros--- ");
+
+                //Menu retiros = new Retiros();
+                    //retiros.Transaccion1();
+                } else if (opcion2 == 3) {
+                    System.out.println("   ---Saldo--- ");
+
+                    Menu saldo = new Saldo();
+                    saldo.Transaccion1();
+
+                } else if (opcion2 == 4) {
+                    System.out.println("   ---Clientes--- ");
+
+                 //Menu clientes = new Clientes();
+                    //Clientes.Transaccion1();
+                } else if (opcion2 == 5) {
+                    System.out.println("---------------------");
+                    System.out.println("Gracias vuelva pronto");
+                    System.out.println("---------------------");
+                    bandera = 2;
+                }
+
+            } while (bandera != 2);
+        } else if (opcion1 == 3) {
+            
+            do {
+
+            //Bucle menú 
+                System.out.println("-----Por favor seleccione una opción-----");
+                System.out.println("    1.Deposito de efectivo");
+                System.out.println("    2.Retiro de dinero");
+                System.out.println("    3.Saldo en la cuenta");
+                System.out.println("    4.Clientes");
+                System.out.println("    5.Salir");
+
+                System.out.println("-----------------------------------------");
+
+                System.out.print("-Opción: ");
+                opcion2 = scn.nextInt();
+
+                if (opcion2 == 1) {
+
+                    System.out.println("   ---Deposito--- ");
+
+                    Menu deposito = new Depositos();
+                    deposito.Transaccion1();
+
+                } else if (opcion2 == 2) {
+                    System.out.println("   ---Retiros--- ");
+
+                //Menu retiros = new Retiros();
+                    //retiros.Transaccion1();
+                } else if (opcion2 == 3) {
+                    System.out.println("   ---Saldo--- ");
+
+                    Menu saldo = new Saldo();
+                    saldo.Transaccion1();
+
+                } else if (opcion2 == 4) {
+                    System.out.println("   ---Clientes--- ");
+
+                 //Menu clientes = new Clientes();
+                    //Clientes.Transaccion1();
+                } else if (opcion2 == 5) {
+                    System.out.println("---------------------");
+                    System.out.println("Gracias vuelva pronto");
+                    System.out.println("---------------------");
+                    bandera = 2;
+                }
+
+            } while (bandera != 2);
+
+            Menu user3 = new Saldo();
+           
+            do {
+
+            //Bucle menú 
+                System.out.println("-----Por favor seleccione una opción-----");
+                System.out.println("    1.Deposito de efectivo");
+                System.out.println("    2.Retiro de dinero");
+                System.out.println("    3.Saldo en la cuenta");
+                System.out.println("    4.Clientes");
+                System.out.println("    5.Salir");
+
+                System.out.println("-----------------------------------------");
+
+                System.out.print("-Opción: ");
+                opcion2 = scn.nextInt();
+
+                if (opcion2 == 1) {
+
+                    System.out.println("   ---Deposito--- ");
+
+                    Menu deposito = new Depositos();
+                    deposito.Transaccion1();
+
+                } else if (opcion2 == 2) {
+                    System.out.println("   ---Retiros--- ");
+
+                //Menu retiros = new Retiros();
+                    //retiros.Transaccion1();
+                } else if (opcion2 == 3) {
+                    System.out.println("   ---Saldo--- ");
+
+                    Menu saldo = new Saldo();
+                    saldo.Transaccion1();
+
+                } else if (opcion2 == 4) {
+                    System.out.println("   ---Clientes--- ");
+
+                 //Menu clientes = new Clientes();
+                    //Clientes.Transaccion1();
+                } else if (opcion2 == 5) {
+                    System.out.println("---------------------");
+                    System.out.println("Gracias vuelva pronto");
+                    System.out.println("---------------------");
+                    bandera = 2;
+                }
+
+            } while (bandera != 2);
+
+        }
+
+    }
 
     public int getSaldo1() {
         return Saldo1;
@@ -76,13 +258,20 @@ public abstract class Menu {
     public void setSaldo3(int Saldo3) {
         this.Saldo3 = Saldo3;
     }
-
-    
-    
+/*
     protected double depositos;
 
     public void Depositos() {
         depositos = scn.nextDouble();
     }
-
+*/
+    public void Deposito1(){
+        deposito1=scn.nextInt();
+    }
+     public void Deposito2(){
+        deposito2=scn.nextInt();
+    }
+      public void Deposito3(){
+        deposito3=scn.nextInt();
+    }
 }
